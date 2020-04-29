52a198cf5d1f1403c82bf94a760f9e63da37836f
jos3duardo <jos3duardolopes@gmail.com>
2020-04-29 18:32:43 -0400

    generate auth files

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/message', function (){
     return view('message');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
